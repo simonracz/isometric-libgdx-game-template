c488cfef7c2840532506707e2082ffd45d540db3
Simon Racz <simonracz@gmail.com>
2013-08-18 21:30:41 +0200

    Added bg assets, ETC1 compressor. Manifest changes.

--- isometric-game-template-desktop/src/com/chaonis/isometric/Main.java
+++ isometric-game-template-desktop/src/com/chaonis/isometric/Main.java
@@ -7,9 +7,9 @@ public class Main {
 	public static void main(String[] args) {
 		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
 		cfg.title = "isometric-game-template";
-		cfg.useGL20 = false;
-		cfg.width = 480;
-		cfg.height = 320;
+		cfg.useGL20 = true;
+		cfg.width = 1280;
+		cfg.height = 720;
 		
 		new LwjglApplication(new Isometric(), cfg);
 	}
