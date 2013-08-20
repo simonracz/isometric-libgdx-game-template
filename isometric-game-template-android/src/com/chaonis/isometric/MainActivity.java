5245e50cea846a63a122554b44772d91deec9b05
Simon Racz <simonracz@gmail.com>
2013-08-20 17:13:50 +0200

    Added more assets.

--- isometric-game-template-android/src/com/chaonis/isometric/MainActivity.java
+++ isometric-game-template-android/src/com/chaonis/isometric/MainActivity.java
@@ -11,7 +11,9 @@ public class MainActivity extends AndroidApplication {
         super.onCreate(savedInstanceState);
         
         AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
-        cfg.useGL20 = false;
+        cfg.useGL20 = true;
+        cfg.useAccelerometer = false;
+        cfg.useCompass = false;
         
         initialize(new Isometric(), cfg);
     }
